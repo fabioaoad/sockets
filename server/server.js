f644a2b57b7e7d2aa3e7c1256596454f7a82ebb2
Fabio <fabioaoad@gmail.com>
2021-12-09 15:51:40 -0300

    Emitir desde el servidor - Escuchar en el cliente - S14A165

--- server/server.js
+++ server/server.js
@@ -19,6 +19,12 @@ let io = socketIO(server);
 io.on('connection', (client)=>{
     console.log('Usuario conectado');
 
+    client.emit('enviarMensaje', {
+        usuario: 'Administrador',
+        mensaje: 'Bienvenido a esta aplicación'
+    });
+
+
     // Cuando cierra el navegador el cliente o cierra la pestaña
     client.on('disconnect', ()=>{
         console.log('Usuario desconectado');
